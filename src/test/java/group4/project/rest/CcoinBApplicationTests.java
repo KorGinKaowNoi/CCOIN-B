3d3c176efb8a797076d645299d7dc11cc9047fb1
mineteeraa <patteera_mine@hotmail.com>
2022-01-18 21:24:39 +0700

    initial commit

---
+++ src/test/java/group4/project/rest/CcoinBApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.project.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcoinBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
