01d6e9a6390aac9d6ee7270a78a27196026d1cd0
Mukiisanma1412 <supatsara_r@cmu.ac.th>
2022-02-04 15:54:29 +0700

    Add Sell in controller

--- src/main/java/group4/project/rest/controller/UserController.java
+++ src/main/java/group4/project/rest/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class UserController {
@@ -19,5 +20,13 @@ public class UserController {
     public ResponseEntity<?> getUserProfileById(@PathVariable("id") Long id){
         User user = userRepository.findById(id).get();
         return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(user));
+    }
+
+    @PutMapping("/user/{id}/sell")
+    public ResponseEntity<?> sellCoin(@PathVariable("id") Long id){
+        User user = userRepository.findById(id).get();
+        user.setCoin(user.getCoin()-1);
+        final User updateCoins = userRepository.save(user);
+        return ResponseEntity.ok(updateCoins);
     }
 }
