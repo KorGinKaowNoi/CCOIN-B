4b766337ce01cfd6cc5c630b13279bb950503564
mineteeraa <patteera_mine@hotmail.com>
2022-02-08 01:18:42 +0700

    Adding COR configuration

--- src/main/java/group4/project/rest/CcoinBApplication.java
+++ src/main/java/group4/project/rest/CcoinBApplication.java
@@ -2,6 +2,9 @@ package group4.project.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CcoinBApplication {
@@ -10,4 +13,15 @@ public class CcoinBApplication {
         SpringApplication.run(CcoinBApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000/")
+                        .exposedHeaders("x-total-count");
+            }
+        };
+    }
 }
