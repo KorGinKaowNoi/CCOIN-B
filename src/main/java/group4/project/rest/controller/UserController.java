82ffe0f4f4d13619bb4fbee182900cb1fa279443
KorGinKaowNoi <thitisan_ch@cmu.ac.th>
2022-02-08 22:27:16 +0700

    add condition to buy coin

--- src/main/java/group4/project/rest/controller/UserController.java
+++ src/main/java/group4/project/rest/controller/UserController.java
@@ -1,8 +1,10 @@
 package group4.project.rest.controller;
 
 
+import group4.project.rest.entity.CamtCoin;
 import group4.project.rest.entity.StudentBuyCoinDTO;
 import group4.project.rest.entity.StudentSellCoinDTO;
+import group4.project.rest.repository.CamtCoinRepository;
 import group4.project.rest.security.entity.User;
 import group4.project.rest.security.repository.UserRepository;
 import group4.project.rest.util.LabMapper;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    CamtCoinRepository camtCoinRepository;
 
     @GetMapping("/user/{id}")
     public ResponseEntity<?> getUserProfileById(@PathVariable("id") Long id){
@@ -40,13 +44,17 @@ public class UserController {
     @PutMapping("/user/{id}/buy")
     public ResponseEntity<?> buyCoin(@PathVariable("id") Long id, @RequestBody StudentBuyCoinDTO buyer){
         User user = userRepository.findById(id).get();
+        CamtCoin coin = camtCoinRepository.getById(1L);
+        Integer coinLeft = coin.getCoinPerDay();
         Double price = buyer.getAmount()*10.0;
-        if(user.getBalance()<price){
+        if(user.getBalance()<price || buyer.getAmount() > coinLeft){
             return ResponseEntity.ok(false);
         }
         user.setBalance(user.getBalance()-price);
         user.setCoin(user.getCoin()+buyer.getAmount());
+        coin.setCoinPerDay(coinLeft-buyer.getAmount());
         userRepository.save(user);
+        camtCoinRepository.save(coin);
         return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(user));
     }
 }
